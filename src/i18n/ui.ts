9b229d36ffc64961be9874205bc831a5863d33f3
kimberlly florez <kimberlly.florez@gmail.com>
2023-12-10 10:21:58 +1100

    More things

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -36,6 +36,13 @@ export const ui = {
     "aboutUs.laura": "Laura",
     "aboutUs.lauraDescription":
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    //Footer
+    "footer.findUs": "find us",
+    "footer.adress": "62, Tunja-Motavita, Boyaca Colombia",
+    "footer.phonetitle": "Contact us",
+    "footer.phone": "+573208890057",
+    "footer.email": "Albalusanchezramos@gmail.com",
+    "footer.emailTitle": "Albalusanchezramos@gmail.com"
   },
   es: {
     "nav.home": "INICIO",
@@ -46,5 +53,12 @@ export const ui = {
     "index.heroMessage":
       "Transformamos tus momentos especiales en experiencias inolvidables. Descubre la magia de celebrar con nosotros y disfruta en nuestro exclusivo alojamiento estilo Airbnb. Donde cada detalle cuenta, creamos recuerdos que perduran.",
     "index.heroActionLabel": "Let's Connect",
+    //Footer
+    "footer.findUs": "Encuentranos",
+    "footer.adress": "62, Tunja-Motavita, Boyaca Colombia",
+    "footer.phonetitle": "Contact us",
+    'footer.phone': "+573208890057",
+    "footer.email": "Escribenos",
+    "footer.emailTitle": "Albalusanchezramos@gmail.com"
   },
 } as const;
