defa8be3bab91d6cb3e002f716abb4417d02e607
Cesar Urrego <caurregoz.is@gmail.com>
2023-12-09 20:37:48 +1100

    Add i18n and generic hero card

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "HOME",
+    "nav.about": "ABOUT US",
+    "nav.contact": "CONTACT",
+  },
+  fr: {
+    "nav.home": "INICIO",
+    "nav.about": "NOSOTROS",
+    "nav.contact": "CONTACTANOS",
+  },
+} as const;
