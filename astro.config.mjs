21ce8cf86482fd033ffc91ed713e1fe12f5ba0cb
Cesar Urrego <caurregoz.is@gmail.com>
2023-12-09 22:24:11 +1100

    Update page base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kimberllyflorez.github.io",
-  base: "/squishy",
+  base: "/dawnfer_web_page",
 });
