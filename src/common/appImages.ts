d3a3694a8f1be564bcb3d0314f768c8dc1678b15
kimberlly florez <kimberlly.florez@gmail.com>
2023-12-10 15:46:20 +1100

    feat: gallery section

--- src/common/appImages.ts
+++ src/common/appImages.ts
@@ -2,4 +2,11 @@ export const AppImages = {
   marriedDecoration: "src/img/married.jpeg",
   luciaPhoto: "https://randomuser.me/api/portraits/women/79.jpg",
   lauraPhoto: "https://randomuser.me/api/portraits/women/31.jpg",
+  gallerytheme: "src/img/gallery/tematic.jpeg",
+  galleryfifty:"src/img/gallery/years.jpeg",
+  gallerymarried:"src/img/gallery/married.jpeg",
+galleryCake: "src/img/gallery/cake.jpeg",
+gallerybaby: "src/img/gallery/babyshower.jpeg",
+
+
 };
